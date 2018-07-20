bdee5a37a10943d44f2382e0f24a02ec912d57aa
cillyfly <cillyfly@163.com>
2018-07-20 11:54:15 +0800

    replace img

---
+++ Example/MXScroll/MXExampleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MXExampleViewController.swift
+//  MXScroll_Example
+//
+//  Created by cc x on 2018/7/20.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class MXExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
